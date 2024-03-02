4a3ddcb9f7df82c1fa982a9cf65597dd2520178b
Gabriel <caampos22@gmail.com>
2024-03-01 23:51:02 -0300

    Feat 🎉: Add a visual response for the user.
    For better user experience, we added a loading spinner to the login button
    and disabled the form while it is submitting.
    That way, the user have a quick visual response and prevent another request
    while the first one is still processing.

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -4,7 +4,9 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { ReloadIcon } from '@radix-ui/react-icons'
 import { signIn } from 'next-auth/react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import z from 'zod'
@@ -14,20 +16,32 @@ const formSchema = z.object({
 })
 
 export function AuthForm() {
+	const [isSubmitting, setIsSubmitting] = useState(false)
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 	})
 
 	const handleSubmit = form.handleSubmit(
 		async (data: z.infer<typeof formSchema>) => {
+			setIsSubmitting(true)
 			try {
 				await signIn('email', { email: data.email, redirect: false })
+				setIsSubmitting(false)
 
 				toast('Magic Link Sent 🎉', {
 					description: 'Check your email for the magic link to login',
 				})
 			} catch (err) {
+				setIsSubmitting(false)
 				console.error(err)
+				toast('We had a problem! 😥', {
+					description:
+						'We had a problem sending your magic link, please try again.',
+					action: {
+						label: 'Close',
+						onClick: () => toast.dismiss(),
+					},
+				})
 			}
 		},
 	)
@@ -49,12 +63,30 @@ export function AuthForm() {
 							placeholder='m@example.com'
 							required
 							type='email'
+							disabled={isSubmitting}
 							{...form.register('email')}
 						/>
 					</div>
-					<Button className='w-full' type='submit'>
-						Send magic link
+					<Button className='w-full flex' type='submit' disabled={isSubmitting}>
+						<span className='ml-2'>Send Magic Link</span>
+						<ReloadIcon
+							aria-disabled={!isSubmitting}
+							className='size-4 animate-spin ml-2 aria-disabled:opacity-0'
+						/>
 					</Button>
+					<div>
+						<p className='text-xs text-gray-400 text-center	'>
+							By signing in, you agree to our{' '}
+							<a href='/' className='underline underline-offset-2'>
+								Terms of Service
+							</a>{' '}
+							and{' '}
+							<a href='/' className='underline underline-offset-2'>
+								Privacy Policy
+							</a>
+							.
+						</p>
+					</div>
 				</form>
 			</div>
 		</div>
