46d28a27f043668053dcf2340909ab7c0ea6c888
Gabriel <caampos22@gmail.com>
2024-03-04 03:04:44 -0300

    Fix 👨🏻‍🔧: Fix middleware
    The if statement was throwing too many redirects because of the form that was written.
    To fix it we separated in two different statements.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,10 @@ export default function middleware(request: NextRequest) {
 	const token = request.cookies.get('authjs.session-token')
 	const pathname = request.nextUrl.pathname
 
-	if (pathname === '/auth' || (pathname === '/' && token)) {
+	if (pathname === '/auth' && token) {
+		return NextResponse.redirect(new URL(getUrl('/app')))
+	}
+	if (pathname === '/' && token) {
 		return NextResponse.redirect(new URL(getUrl('/app')))
 	}
 
