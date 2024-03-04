8dec367bb9b9620bfc9b7e4a7b651fce850cc0c7
Gabriel <caampos22@gmail.com>
2024-03-04 02:39:33 -0300

    Fix 👨🏻‍🔧: Fix svg proprieties
    Change the icons proprieties into camelCase format.

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -88,9 +88,9 @@ export function NotificationsOnIcon(props: React.SVGProps<SVGSVGElement>) {
 			viewBox='0 0 24 24'
 			fill='none'
 			stroke='currentColor'
-			stroke-linecap='round'
-			stroke-linejoin='round'
-			stroke-width='1.25'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth='1.25'
 		>
 			<path d='M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9M10.3 21a1.94 1.94 0 0 0 3.4 0' />
 		</svg>
