df56b5ad104276bc5d46dbb5d44db5de3d781e3b
Gabriel <caampos22@gmail.com>
2024-03-13 13:23:20 -0300

    Fix 👨🏻‍🔧: Fix fetching flashcards on all subcategories
    Fixed not being able to fetch flashcards inside a deck's subcategories.
    Fix issue #5.

--- src/app/api/flashcards/fetch-cards/route.ts
+++ src/app/api/flashcards/fetch-cards/route.ts
@@ -1,6 +1,12 @@
 import { auth } from '@/services/auth'
 import { prisma } from '@/services/database'
+import { Category, Flashcard } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+
+type CategoryWithFlashcards = Category & {
+	flashcards: Flashcard[]
+	subCategories?: CategoryWithFlashcards[]
+}
 
 export async function GET(req: NextRequest) {
 	const categoryId = req.nextUrl.searchParams.get('category')
@@ -12,17 +18,70 @@ export async function GET(req: NextRequest) {
 				message: 'User not authenticated',
 			})
 
-		const flashcards = await prisma.flashcard.findMany({
-			where: {
-				userId: session.user?.id,
-				categoryId: categoryId || undefined,
-				nextReviewDate: {
-					lt: new Date(),
+		if (!categoryId) return
+
+		const category = await prisma.category.findUnique({
+			include: {
+				flashcards: true,
+				subCategories: {
+					include: {
+						flashcards: true,
+						subCategories: {
+							include: {
+								flashcards: true,
+								subCategories: {
+									include: {
+										flashcards: true,
+										subCategories: {
+											include: {
+												flashcards: true,
+												subCategories: {
+													include: {
+														flashcards: true,
+														subCategories: {
+															include: {
+																flashcards: true,
+																subCategories: {
+																	include: {
+																		flashcards: true,
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
 				},
+			},
+			where: {
+				id: categoryId,
+				userId: session.user?.id,
 			},
 		})
 
-		prisma.$disconnect()
+		if (!category) return NextResponse.json({ message: 'Category not found' })
+
+		const flashcards = category.flashcards.filter((flashcard) => {
+			return new Date(flashcard.nextReviewDate) <= new Date()
+		})
+
+		const includeSubcategories = (categories?: CategoryWithFlashcards[]) => {
+			if (!categories) return
+
+			for (const category of categories) {
+				flashcards.push(...category.flashcards)
+				includeSubcategories(category.subCategories)
+			}
+		}
+
+		includeSubcategories(category.subCategories)
+
 		return NextResponse.json({ success: true, flashcards })
 	} catch (error) {
 		return NextResponse.json({ message: 'Error fetching flashcards', error })
