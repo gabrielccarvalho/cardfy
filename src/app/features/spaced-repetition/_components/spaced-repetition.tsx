6f9566c906d257874421041bbd9a63a1bab52994
Gabriel <caampos22@gmail.com>
2024-03-20 16:31:02 -0300

    Feat 🎉: Add information on spaced-repetition page
    Add missing information on the features/spaced-repetition page. Contains some illustrations and a brief description.

--- src/app/features/spaced-repetition/_components/spaced-repetition.tsx
+++ src/app/features/spaced-repetition/_components/spaced-repetition.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button'
 import { MaxWidthWrapper } from '@/components/ui/max-width-wrapper'
 import { FADE_IN_ANIMATION_SETTINGS } from '@/lib/framer-motion'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export function SpacedRepetitionFeature() {
@@ -43,26 +44,90 @@ export function SpacedRepetitionFeature() {
 			<MaxWidthWrapper className='mt-10 grid gap-4 md:grid-cols-2'>
 				<motion.div
 					{...FADE_IN_ANIMATION_SETTINGS}
-					className='h-96 bg-white rounded-2xl shadow-lg border border-border'
-				/>
+					className='h-96 bg-white rounded-2xl shadow-lg border border-border p-6 flex flex-col justify-between'
+					style={{
+						background:
+							'radial-gradient(circle at center, white 50%, #f1f5f9 80%)',
+					}}
+				>
+					<div className='h-96 w-full flex items-center justify-center'>
+						<Image
+							src='/assets/features/activity-calendar.svg'
+							alt='Activity calendar'
+							width={400}
+							height={400}
+							draggable={false}
+							className='mx-auto'
+						/>
+					</div>
+					<div className='w-full flex items-center flex-col space-y-1 max-w-sm text-center mx-auto'>
+						<h4 className='text-xl font-semibold'>Activity Calendar</h4>
+						<p className='text-gray-600'>
+							Cardfy gives you a clear view of when your cards are due and how
+							many each day.
+						</p>
+					</div>
+				</motion.div>
 				<motion.div
 					{...FADE_IN_ANIMATION_SETTINGS}
-					className='h-96 bg-white rounded-2xl shadow-lg border border-border'
-				/>
+					className='h-96 bg-white rounded-2xl shadow-lg border border-border py-6 flex flex-col justify-between'
+					style={{
+						background:
+							'radial-gradient(circle at center, white 50%, #f1f5f9 80%)',
+					}}
+				>
+					<div className='h-72 w-full flex items-center justify-center'>
+						<Image
+							src='/assets/features/due-dates.svg'
+							alt='Activity calendar'
+							width={900}
+							height={400}
+							draggable={false}
+							className='mx-auto'
+						/>
+					</div>
+					<div className='w-full flex items-center flex-col space-y-1 max-w-sm text-center mx-auto'>
+						<h4 className='text-xl font-semibold'>Due Dates</h4>
+						<p className='text-gray-600'>
+							Cardfy gives you a clear view of when your cards are due and how
+							many each day.
+						</p>
+					</div>
+				</motion.div>
 			</MaxWidthWrapper>
 			<MaxWidthWrapper className='mt-4 grid gap-4 md:grid-cols-3'>
 				<motion.div
 					{...FADE_IN_ANIMATION_SETTINGS}
-					className='h-32 bg-white rounded-2xl shadow-lg border border-border'
-				/>
+					className='h-36 items-center flex flex-col justify-center text-center bg-white rounded-2xl shadow-lg border border-border gap-1 px-4'
+				>
+					<span className='text-xl font-medium'>Spaced repetition</span>
+					<p className='text-gray-500 text-md'>
+						Use the best algorithm to learn and retain your knowledge.
+					</p>
+				</motion.div>
 				<motion.div
 					{...FADE_IN_ANIMATION_SETTINGS}
-					className='h-32 bg-white rounded-2xl shadow-lg border border-border'
-				/>
+					className='h-36 items-center flex flex-col justify-center text-center bg-white rounded-2xl shadow-lg border border-border gap-1 px-4'
+				>
+					<span className='text-xl font-medium'>
+						Intuitive graduation system
+					</span>
+					<p className='text-gray-500 text-md'>
+						Graduate your cards based on the easiness with 4 simple options.
+					</p>
+				</motion.div>
 				<motion.div
 					{...FADE_IN_ANIMATION_SETTINGS}
-					className='h-32 bg-white rounded-2xl shadow-lg border border-border'
-				/>
+					className='h-36 items-center flex flex-col justify-center text-center bg-white rounded-2xl shadow-lg border border-border gap-1 px-4'
+				>
+					<span className='text-xl font-medium'>
+						Automatic review sugestion
+					</span>
+					<p className='text-gray-500 text-md'>
+						Cardfy suggest the best time to review your cards based on your
+						learning.
+					</p>
+				</motion.div>
 			</MaxWidthWrapper>
 		</main>
 	)
